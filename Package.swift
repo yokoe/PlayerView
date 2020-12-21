f284c6d1781c98fee45309b2e94ba6ef60e3de4c
yokoe <kreuz45@kreuz45.com>
2020-12-21 09:39:25 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "PlayerView",
+    platforms: [
+        .macOS(.v10_12),
+        .iOS(.v13)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
